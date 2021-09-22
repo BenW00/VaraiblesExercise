874c8b95539c926250e955e5bc6465d42278be81
BenW00 <ispyb2000@gmail.com>
2021-09-21 22:41:40 -0400

    finished exercise

---
+++ Script.js
@@ -0,0 +1,38 @@
+// Question 1
+// const
+
+// Question 2
+let num = 20
+
+// Question 3
+num = 15
+
+// Question 4
+num += 7
+
+// Question 5
+num--;
+
+// Question 6
+num = "This is not a number"
+
+// Question 7
+let blank 
+
+// Question 8
+console.log(blank)
+
+// Question 9
+blank = ""
+
+// Bonus 1
+const quater = 0.25, dime = 0.10, nickle = 0.5, penny = 0.01
+
+// Bonus 2
+console.log(quater,dime,nickle,penny)
+
+// Bonus 3
+let letters = "qwertyuiop"
+
+// bonus 4
+console.log(letters[9])
